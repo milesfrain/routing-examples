e29b3014c2eac60c50d10b9f7ccc7ee2659df39e
Miles Frain <miles.frain@colorado.edu>
2020-06-02 15:34:52 -0600

    push routing working with callback

--- src/Example.purs
+++ src/Example.purs
@@ -2,10 +2,15 @@ module Example where
 
 import Prelude
 import Data.Maybe (Maybe(..))
+import Effect.Class (class MonadEffect, liftEffect)
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import MyRouting (MyRoute)
+import Web.HTML (window)
+import Web.HTML.Location (setHref)
+import Web.HTML.Window (location)
 
 type State
   = Maybe MyRoute
@@ -14,7 +19,10 @@ type State
 data Query a
   = Nav MyRoute a
 
-component :: forall i o m. H.Component HH.HTML Query i o m
+data Action
+  = Redirect
+
+component :: forall i o m. MonadEffect m => H.Component HH.HTML Query i o m
 component =
   H.mkComponent
     { initialState: const Nothing
@@ -23,23 +31,43 @@ component =
         H.mkEval
           $ H.defaultEval
               { handleQuery = handleQuery
+              , handleAction = handleAction
               }
     }
 
-render :: forall a m. State -> H.ComponentHTML a () m
+render :: forall m. State -> H.ComponentHTML Action () m
 render state =
   let
     renderLink link txt = HH.div_ [ HH.a [ HP.href link ] [ HH.text txt ] ]
   in
     HH.div_
       [ HH.text $ show state
-      , renderLink "#posts/" "Index"
-      , renderLink "#posts/8" "View post 8"
-      , renderLink "#posts/8/edit" "Edit post 8"
-      , renderLink "#posts/browse/2004/June" "Browse 2004 June"
+      , renderLink "/posts/" "Index"
+      , renderLink "/posts/8" "View post 8"
+      , renderLink "/posts/8/edit" "Edit post 8"
+      , renderLink "/posts/browse/2004/June" "Browse 2004 June"
+      , HH.div_
+          [ HH.textarea []
+          ]
+      , HH.div_
+          [ HH.button
+              [ HE.onClick \_ -> Just Redirect
+              ]
+              [ HH.text "Save Gist"
+              ]
+          ]
       ]
 
-handleQuery ∷ forall a ac o m. Query a → H.HalogenM State ac () o m (Maybe a)
+handleQuery ∷ forall a o m. Query a → H.HalogenM State Action () o m (Maybe a)
 handleQuery (Nav route a) = do
   H.put $ Just route
   pure (Just a)
+
+handleAction ∷ forall o m. MonadEffect m => Action -> H.HalogenM State Action () o m Unit
+handleAction = case _ of
+  Redirect ->
+    liftEffect
+      $ do
+          win <- window
+          loc <- location win
+          setHref "https://github.com/login/oauth/authorize?client_id=bbaa8fdc61cceb40c899&scope=gist" loc
--- src/Routing.purs
+++ src/Routing.purs
@@ -4,13 +4,15 @@ import Prelude
 import Data.Foldable (oneOf)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
-import Routing.Match (Match, int, lit, str)
+import Routing.Match (Match, int, lit, param, root, str)
 
 type PostId
   = Int
 
 data MyRoute
   = PostIndex
+  | CB String
+  | Callback
   | Post PostId
   | PostEdit PostId
   | PostBrowse Int String
@@ -22,10 +24,17 @@ instance showMyRoute :: Show MyRoute where
 
 myRoute :: Match MyRoute
 myRoute =
-  lit "posts"
+  root
     *> oneOf
-        [ PostEdit <$> int <* lit "edit"
-        , Post <$> int
-        , PostBrowse <$> (lit "browse" *> int) <*> str
-        , pure PostIndex -- Unmatched goes to index too
+        [ lit "posts"
+            *> oneOf
+                [ PostEdit <$> int <* lit "edit"
+                , Post <$> int
+                , PostBrowse <$> (lit "browse" *> int) <*> str
+                , pure PostIndex -- Unmatched goes to index too
+                ]
+        , oneOf
+            [ CB <$> (lit "callback" *> param "code")
+            , Callback <$ lit "callback"
+            ]
         ]
